6ef6b1688cbc9c12f62c2a43f49ea225999d2b88
David-Koshkadze <rootus3rr@pm.me>
2024-08-29 16:12:54 +0400

    delete task from form

--- lib/pm_task_elixir_web/live/task_live/index.ex
+++ lib/pm_task_elixir_web/live/task_live/index.ex
@@ -10,8 +10,27 @@ defmodule PmTaskElixirWeb.Live.TaskLive.Index do
     case Task.create_task(task_params) do
       {:ok, _task} ->
         {:noreply, assign(socket, :tasks, list_tasks())}
+
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  # delete task
+  def handle_event("delete", %{"id" => id}, socket) do
+    task = Task.get_task!(id)
+
+    case Task.delete_task(task) do
+      {:ok, _} ->
+        socket = assign(socket, :tasks, list_tasks())
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Task deleted successfully")
+         |> push_navigate(to: ~p"/tasks")}
+
+      {:error, _} ->
+        {:noreply, put_flash(socket, :error, "Failed to delete task")}
     end
   end
 

db7d8ec4fbd48db3047818399d9e0322041f9fcb
David-Koshkadze <rootus3rr@pm.me>
2024-09-02 22:31:07 +0400

    editing title

--- lib/pm_task_elixir_web/live/task_live/index.ex
+++ lib/pm_task_elixir_web/live/task_live/index.ex
@@ -5,9 +5,16 @@ defmodule PmTaskElixirWeb.Live.TaskLive.Index do
   alias PmTaskElixir.Status
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, tasks: Task.list_tasks(), selected_task: nil, statuses: Repo.all(Status))}
+    {:ok,
+     assign(socket,
+       tasks: Task.list_tasks(),
+       selected_task: nil,
+       statuses: Repo.all(Status),
+       editing_title: false
+     )}
   end
 
+  # --- handle_event ---
   def handle_event("create", %{"task" => task_params}, socket) do
     case Task.create_task(task_params) do
       {:ok, _task} ->
@@ -59,13 +66,35 @@ defmodule PmTaskElixirWeb.Live.TaskLive.Index do
     task = Task.get_task!(id)
     IO.inspect(task.status, label: "Loaded Status")
     Process.send_after(self(), :show_modal, 50)
-    {:noreply, assign(socket, selected_task: task, show_modal: false)}
+    {:noreply, assign(socket, selected_task: task, show_modal: false, editing_title: false)}
   end
 
   def handle_event("hide_modal", _, socket) do
-    {:noreply, assign(socket, selected_task: nil)}
+    {:noreply, assign(socket, selected_task: nil, tasks: list_tasks())}
   end
 
+  # --- editing title ---
+  def handle_event("edit_title", _, socket) do
+    {:noreply, assign(socket, editing_title: true)}
+  end
+
+  def handle_event("cancel_edit_title", _, socket) do
+    {:noreply, assign(socket, editing_title: false)}
+  end
+
+  def handle_event("save_title", %{"title" => title}, socket) do
+    %{selected_task: selected_task} = socket.assigns
+
+    case Task.update_task(selected_task, %{title: title}) do
+      {:ok, updated_task} ->
+        {:noreply, assign(socket, selected_task: updated_task, editing_title: false)}
+
+      {:error, _changeset} ->
+        {:noreply, put_flash(socket, :error, "Failed to update title")}
+    end
+  end
+
+  # --- handle_info ---
   def handle_info(:show_modal, socket) do
     {:noreply, assign(socket, show_modal: true)}
   end
